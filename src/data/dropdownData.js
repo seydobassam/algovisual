33e33385f233732d571c4366f1e72632d41a1cd3
Bassam <seydobassam@gmail.com>
2022-03-25 21:49:08 +0100

    added data object

---
+++ src/data/dropdownData.js
@@ -0,0 +1,102 @@
+export const dropdownData = [
+    {
+      id: "1",
+      name: "Pathfinding",
+      algorithms: [
+        {
+          id: "1",
+          name: "Dijkstra’s Algorithm",
+        },
+        {
+          id: "2",
+          name: "A* Search Algorithm",
+        },
+        {
+          id: "3",
+          name: "Greedy Best-first Search",
+        },
+        {
+          id: "4",
+          name: "Swarm Algorithm",
+        },
+        {
+          id: "5",
+          name: "Breadth First Search",
+        },
+        {
+          id: "6",
+          name: "Depth First Search",
+        },
+      ],
+      virtualViews: [
+        {
+          id: "1",
+          key: "charactersSortView",
+          name: "Pathfinding Grid",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Search",
+      algorithms: [
+        {
+          id: "1",
+          name: "Linear Search",
+        },
+        {
+          id: "2",
+          name: "Binary Search",
+        },
+        {
+          id: "3",
+          name: "Jump Search",
+        },
+        {
+          id: "4",
+          name: "Fibonacci Search",
+        },
+      ],
+      virtualViews: [
+        {
+          id: "1",
+          key: "searchGridView",
+          name: "Search Grid",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "Sort",
+      algorithms: [
+        {
+          id: "1",
+          name: "Selection Sort",
+        },
+        {
+          id: "2",
+          name: "Bubble Sort",
+        },
+        {
+          id: "3",
+          name: "Insertion Sort",
+        },
+        {
+          id: "4",
+          name: "Merge Sort",
+        },
+        {
+          id: "5",
+          name: "Quick Sort",
+        },
+      ],
+      virtualViews: [
+        {
+          id: "1",
+          key: "charactersSortView",
+          name: "Characters",
+        },
+      ],
+    },
+  ];
+  
