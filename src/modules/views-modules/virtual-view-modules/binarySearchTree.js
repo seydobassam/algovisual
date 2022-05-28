4765e3b7124c494c2153829d6a268b46788a8367
Bassam <seydobassam@gmail.com>
2022-05-28 18:03:05 +0200

    create binary tree

--- src/modules/views-modules/virtual-view-modules/binarySearchTree.js
+++ src/modules/views-modules/virtual-view-modules/binarySearchTree.js
@@ -6,15 +6,17 @@ export default function binarySearchTree() {
 
   function initBST() {
     onMounted(() => {
-      createBST(20, 50, 99);
-      createBST(20, 101, 150);
+      createBST(35, 30, 200); // left 
       binaryTreeDrawer().draw("#binarySearchTree", bst);
-      binaryTreeDrawer().onNodeClick((_, n) => {console.log(n);});
+      binaryTreeDrawer().onNodeClick((node) => {
+        console.log(node);
+        // binaryTreeDrawer().animate(node.id);
+      });
     });
   }
 
   function createBST(n, min, max) {
-    if ((max - min) < n) return; 
+    if (max - min < n) return;
     for (let i = 0; i < n; i++) {
       bst.addNode(~~(Math.random() * (max - min + 1)) + min);
     }
