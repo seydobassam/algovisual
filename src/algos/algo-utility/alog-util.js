4a63a4895ff912b830296a5e42f1d9941df55e03
Bassam <seydobassam@gmail.com>
2022-06-08 22:54:22 +0200

    refactor algoutil

--- src/algos/algo-utility/alog-util.js
+++ src/algos/algo-utility/alog-util.js
@@ -1,18 +1,18 @@
-export class AlgoUtil {
+import { NodeType } from "../../constants/Node-type";
 
+export class AlgoUtil {
   static getNodeNeighborsFromGrid(grid, node) {
-    const neighbors = [];
-    const row = node.row;
-    const col = node.col;
-    if (row > 0) neighbors.push(grid[row - 1][col]);
-    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
-    if (col > 0) neighbors.push(grid[row][col - 1]);
-    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
-    return neighbors;
+    const neighbours = [];
+    let { row, col } = node;
+    if (row > 0) neighbours.push(grid[row - 1][col]);
+    if (row < grid.length - 1) neighbours.push(grid[row + 1][col]);
+    if (col > 0) neighbours.push(grid[row][col - 1]);
+    if (col < grid[0].length - 1) neighbours.push(grid[row][col + 1]);
+    return neighbours.filter((neighbour) => neighbour.type !== NodeType.block);
   }
 
   static isNodeAllowed(node) {
-    if (!node.isVisited && node.type !== "block") {
+    if (!node.isVisited) {
       return true;
     }
     return false;
@@ -26,5 +26,13 @@ export class AlgoUtil {
       currentNode = currentNode.previousNode;
     }
     return shortestPathNodes;
+  }
+
+  // Using manhattan distance Heuristic between 2 incomming nodes
+  static getHeuristic(currentNode, finishNode) {
+    return (
+      Math.abs(currentNode.row - finishNode.row) +
+      Math.abs(currentNode.col - finishNode.col)
+    );
   }
 }
